aeb5c78f5862c5080dddb997403577139c962acc
Facundo Luna <facundolunaok@gmail.com>
2023-07-06 22:28:24 -0300

    fix messagesRoute

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -4,5 +4,5 @@ export const loginRoute = `${host}/api/users/login`;
 export const registerRoute = `${host}/api/users/register`;
 export const getUsersRoute = `${host}/api/users`;
 export const chatsRoute = `${host}/api/chats/`;
-export const messagesRoute = `${host}/api/messages/`;
+export const messagesRoute = `${host}/api/messages`;
 export const findUserRoute = `${host}/api/users/find`;
